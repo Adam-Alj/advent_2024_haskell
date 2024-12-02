56dc6712a67e6f2000aaaffe61061d1700648d8a
adamalj <adamaljumaily@gmail.com>
2024-12-01 21:06:14 -0800

    day 1 solution

---
+++ day1/q1.hs
@@ -0,0 +1,22 @@
+import Data.List (sort)
+-- read file into two lists, right and left cols
+-- sort both lists, can we do this during read? some buffer or something?
+-- iterate through both lists, taking the abs value of each diff
+
+lineToPair :: String -> (Int, Int)
+lineToPair line = 
+    let [left, right] = words line
+    in (read left, read right)
+
+readFileToSortedLists :: IO ([Int], [Int])
+readFileToSortedLists = do
+    content <- readFile "./q1input.txt"
+    let linesOfFile = lines content
+    let leftList = [fst $ lineToPair a | a <- linesOfFile ]
+    let rightList = [snd $ lineToPair a | a <- linesOfFile ]
+    return(sort leftList, sort rightList)
+
+main :: IO () 
+main = do 
+    (list1, list2)<- readFileToSortedLists -- O(n log(n)) time estimate
+    print $ sum $ zipWith (\a b -> abs (a-b)) list1 list2
