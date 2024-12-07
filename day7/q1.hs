dea47835fc42a7309cfdddb9404a79853f01247f
adamalj <adamaljumaily@gmail.com>
2024-12-07 12:27:54 -0800

    day7q1

---
+++ day7/q1.hs
@@ -0,0 +1,26 @@
+splitInputLine :: String -> (String, String)
+splitInputLine s =
+    splitAt (length (takeWhile (/= ':') s)) s
+
+processInputLine :: String -> (Int, [Int])
+processInputLine s = do
+    let (testVal, rest) = splitInputLine s
+    (read testVal, map read (words (tail rest)))
+
+lineValid :: (Int, [Int]) -> Bool
+lineValid (target, numbers) =
+    isEquationPossiblyValid target 0 numbers (+) || isEquationPossiblyValid target 1 numbers (*)
+
+isEquationPossiblyValid :: Int -> Int -> [Int] -> (Int -> Int -> Int) -> Bool
+isEquationPossiblyValid target total numbers operation
+    | null numbers = target == total
+    | total > target = False
+    | otherwise = isEquationPossiblyValid target (total `operation` head numbers) (tail numbers) (+) || isEquationPossiblyValid target (total `operation` head numbers) (tail numbers) (*)
+
+main :: IO ()
+main = do
+    fileInput <- readFile "input.txt"
+    let inputLines = map processInputLine (lines fileInput)
+    print $ foldl (\acc (_,a) -> acc + a) 0 (filter fst (zip (map lineValid inputLines) (map fst inputLines)))
+
+-- works for q1 hoooooray
